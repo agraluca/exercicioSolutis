0f5ceb604a7207a64c079171a4745582e5776fdb
Luca Agra <agraluca@SLT-002272.solutis.net.br>
2020-12-16 12:29:48 -0300

    15_dez

--- 14_dez/connectionDb.js
+++ 14_dez/connectionDb.js
@@ -1,23 +1,22 @@
-// import MC from "mongodb";
-// import OI from "mongodb";
-// import env from "dotenv";
+import MC from "mongodb";
+import OI from "mongodb";
+import env from "dotenv";
 
-// function connectionMongo() {
-//   console.log("teste");
-//   env.config();
+function connectionMongo() {
+  console.log("teste");
+  env.config();
 
-//   // DB config
-//   const { MongoClient } = MC;
-//   const { ObjectId } = OI;
+  // DB config
+  const { MongoClient } = MC;
 
-//   MongoClient.connect(
-//     process.env.DB_CONNECTION,
-//     { useNewUrlParser: true, useUnifiedTopology: true },
-//     (err, client) => {
-//       if (err) return console.log(err);
-//       global.db = client.db("solutis");
-//     }
-//   );
-// }
+  global.connection = MongoClient.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      if (err) return console.log(err);
+      global.db = client.db("solutis");
+    }
+  );
+}
 
-// export default connectionMongo;
+export default connectionMongo;
--- 14_dez/server.js
+++ 14_dez/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
-import MC from "mongodb";
-import OI from "mongodb";
 import env from "dotenv";
+import routes from "./src/routes.js";
+import connectionMongo from "./connectionDb.js";
 
 // App config
-let db;
 env.config();
-const port = 3000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 const app = express();
 
 // Ejs
@@ -19,158 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // DB config
-const { MongoClient } = MC;
-const { ObjectId } = OI;
-MongoClient.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) return console.log(err);
-    db = client.db("solutis");
-  }
-);
+connectionMongo();
 
-// Routes User
-
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.post("/show", (req, res) => {
-  db.collection("data").insertOne(req.body, (err, result) => {
-    if (err) return console.log(err);
-
-    console.log("Salvo no Banco de Dados");
-    res.redirect("/show");
-  });
-});
-
-app.get("/", (req, res) => {
-  var cursor = db.collection("data").find();
-});
-
-app.get("/show", (req, res) => {
-  db.collection("data")
-    .find()
-    .toArray((err, results) => {
-      if (err) return console.log(err);
-      res.render("show.ejs", { data: results });
-    });
-});
-
-app
-  .route("/edit/:id")
-  .get((req, res) => {
-    var id = req.params.id;
-
-    db.collection("data")
-      .find(ObjectId(id))
-      .toArray((err, result) => {
-        if (err) return res.send(err);
-        res.render("edit.ejs", { data: result });
-      });
-  })
-  .post((req, res) => {
-    var id = req.params.id;
-    var name = req.body.name;
-    var surname = req.body.surname;
-
-    db.collection("data").updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: {
-          name: name,
-          surname: surname,
-        },
-      },
-      (err, result) => {
-        if (err) return res.send(err);
-        res.redirect("/show");
-        console.log("Atualizado no Banco de Dados");
-      }
-    );
-  });
-
-app.route("/delete/:id").get((req, res) => {
-  var id = req.params.id;
-
-  db.collection("data").deleteOne({ _id: ObjectId(id) }, (err, result) => {
-    if (err) return res.send(500, err);
-    console.log("Deletado do Banco de Dados!");
-    res.redirect("/show");
-  });
-});
-
-// Routes Produtos
-
-app.get("/product", (req, res) => {
-  res.render("index_product.ejs");
-});
-
-app.post("/show_product", (req, res) => {
-  db.collection("product").insertOne(req.body, (err, result) => {
-    if (err) return console.log(err);
-
-    console.log("Salvo no Banco de Dados");
-    res.redirect("/show_product");
-  });
-});
-
-app.get("/product", (req, res) => {
-  var cursor = db.collection("product").find();
-});
-
-app.get("/show_product", (req, res) => {
-  db.collection("product")
-    .find()
-    .toArray((err, results) => {
-      if (err) return console.log(err);
-      res.render("show_product.ejs", { data: results });
-    });
-});
-
-app
-  .route("/edit_product/:id")
-  .get((req, res) => {
-    var id = req.params.id;
-
-    db.collection("product")
-      .find(ObjectId(id))
-      .toArray((err, result) => {
-        if (err) return res.send(err);
-        res.render("edit_product.ejs", { data: result });
-      });
-  })
-  .post((req, res) => {
-    var id = req.params.id;
-    var product = req.body.product;
-    var price = req.body.price;
-
-    db.collection("product").updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: {
-          product,
-          price,
-        },
-      },
-      (err, result) => {
-        if (err) return res.send(err);
-        res.redirect("/show_product");
-        console.log("Atualizado no Banco de Dados");
-      }
-    );
-  });
-
-app.route("/delete_product/:id").get((req, res) => {
-  var id = req.params.id;
-
-  db.collection("product").deleteOne({ _id: ObjectId(id) }, (err, result) => {
-    if (err) return res.send(500, err);
-    console.log("Deletado do Banco de Dados!");
-    res.redirect("/show_product");
-  });
-});
+// Routes
+app.use(routes);
 
 // Listener
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
